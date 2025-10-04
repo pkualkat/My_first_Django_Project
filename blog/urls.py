005a9447dd010981b3760589b19a87a7fcbd3521
pkualkat <pkualkat@ivytech.edu>
2025-10-04 11:44:43 -0400

    Url update.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
-from django.urls import path
-from . import views
+
+from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),  # Connect your app here
 ]
